a72e6bb50c8b170bfdb969dd1d87858236ebae84
baughmanaustin <climbing4him@gmail.com>
2017-02-27 14:54:13 -0700

    Add files via upload

---
+++ Lab 4.py
@@ -0,0 +1,18 @@
+print('Welcome to Binary-Decimal Converter')
+binary_number = input('Please enter an 8 digit binary number: ')
+
+binary_number_list = list(binary_number)
+
+print(binary_number)
+print(binary_number_list)
+
+
+
+
+
+
+
+
+
+
+
